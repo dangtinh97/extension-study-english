40c21c775d12fd76346bbc9ba8b683d4fddd9970
Dang Tinh <>
2023-06-22 05:02:20 +0700

    update

--- js/content-script.js
+++ js/content-script.js
@@ -1,18 +1,12 @@
 let extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
 if (!location.ancestorOrigins.contains(extensionOrigin)) {
-    document.addEventListener("DOMContentLoaded",function (){
-        console.log(window.localStorage.getItem('time_show'),'2')
-    })
-
-    var iframe = document.createElement('iframe');
+    const iframe = document.createElement('iframe');
     // Must be declared at web_accessible_resources in manifest.json
     iframe.src = chrome.runtime.getURL('iframe.html');
     // Some styles for a fancy sidebar
     iframe.style.cssText = 'position:fixed;bottom:10px;right:10px;display:block;' +
-        'z-index:9999; border:none;height:100%;max-width:30%;width:100%';
-
+        'z-index:9999; border:none;height:-webkit-fill-available;max-width:30%;width:100%';
     iframe.className = 'iframe_extension'
-
     document.body.appendChild(iframe);
 }
 //position: fixed !important; max-width: 100% !important; max-height: 100% !important; background-color: transparent !important; z-index: 2147483647 !important; overflow: hidden !important; visibility: visible !important; border: none !important; display: block !important; color-scheme: normal !important; inset: 0px 15px 0px 0px !important; width: calc(100% - 15px) !important; height: 100% !important;
@@ -20,8 +14,13 @@ if (!location.ancestorOrigins.contains(extensionOrigin)) {
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
     console.log(request, sender, sendResponse);
     sendResponse('我收到你的消息了：'+JSON.stringify("request"));
+    const iframe = document.getElementsByClassName('iframe_extension')[0];
     if(request.action==='remove_iframe'){
         // document.getElementsByClassName('iframe_extension').html('')
-        document.getElementsByClassName('iframe_extension')[0].remove()
+        iframe.remove()
+    }
+    if(request.action==='set_width'){
+        iframe.style.width = request.width+'px'
+        iframe.style.height = request.height+'px'
     }
 });
--- js/iframe.js
+++ js/iframe.js
@@ -1,17 +1,59 @@
 document.addEventListener("DOMContentLoaded",function (){
     $.ajax({
-        url:'https://youpip.net/api/login',
+        url:'../data1.json',
         type:"GET",
         success:(res)=>{
-            console.log(res)
+            const index = _.random(0,res.length-1)
+            const data = res[index]
+            $('#word').html(data.word)
+            $('#mean').html(data.mean)
+            console.log({
+                action:'set_width',
+                width:document.getElementsByClassName('content')[0].offsetWidth,
+                height:document.getElementsByClassName('content')[0].offsetHeight,
+                height2:document.getElementsByClassName('content')[0].clientHeight,
+            })
+            setTimeout(()=>{
+                sendToContentScript({
+                    action:'set_width',
+                    width:document.getElementsByClassName('content')[0].offsetWidth+2,
+                    height:document.getElementsByClassName('content')[0].clientHeight
+                })
+                setTimeout(()=>{
+
+                    $('.content').css('opacity',1)
+                },200)
+            },1000)
         }
+
     })
-    async function sendToContentScript(action) {
+
+    $('.content').on('click',()=>{
+        sendToContentScript({
+            action:'remove_iframe'
+        })
+    })
+
+    async function sendToContentScript(request) {
         const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-        chrome.tabs.sendMessage(tab.id, {action: action}, function(response) {
+        chrome.tabs.sendMessage(tab.id, {...request}, function(response) {
             console.log(response)
         });
     }
 })
 
-
+// let result = []
+// for(let i=9;i<20;i++){
+//     const ul = document.querySelector(`#content > article > div.post-content > div.entry-content > ul:nth-child(${i})`)
+//     if(ul!==null){
+//         const li = ul.getElementsByTagName('li')
+//         for(let i=0;i<li.length;i++){
+//             let text = li[i].textContent;
+//             result.push({
+//                 word: text.split(': ')[0],
+//                 mean: text.split(': ')[1]
+//             })
+//         }
+//     }
+// }
+// console.log(JSON.stringify(result))
